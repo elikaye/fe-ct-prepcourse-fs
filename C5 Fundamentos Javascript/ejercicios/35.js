ee6daedb47e399a75eb2721dbc53537afc76c613
elikaye <elianakaye13@gmail.com>
2024-12-16 20:56:27 -0300

    Ej35

--- C5 Fundamentos Javascript/ejercicios/35.js
+++ C5 Fundamentos Javascript/ejercicios/35.js
@@ -1,12 +1,7 @@
 // ⛔️ Recuerda que debes utilizar el objeto global "Math".
 
 function redondearHaciaArriba(num) {
-  // La función recibe un argumento llamado num el cual es un numero.
-  // Debes redondear "num" hacia arriba y retórnalo.
-  // Por ejemplo: 
-  // 2.5 ---> 3
-  // 0.1 ---> 1
-  // Tu código:
+  return Math.ceil(num);
 }
 
 module.exports = redondearHaciaArriba;
