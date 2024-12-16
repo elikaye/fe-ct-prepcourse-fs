6b1549d4b79000aae6d7cb38fc40c77f145819c9
elikaye <elianakaye13@gmail.com>
2024-12-16 18:36:07 -0300

    Ej17

--- C5 Fundamentos Javascript/ejercicios/17.js
+++ C5 Fundamentos Javascript/ejercicios/17.js
@@ -1,11 +1,6 @@
 function esPositivo(num) {
-  // La función recibe un argumento llamado num el cual es un numero entero.
-  // Retorna como resultado un string que indica si el número es positivo o negativo.
-  // Por ejemplo: 
-  // Si el número es positivo ---> "Es positivo".
-  // Si el número es negativo ---> "Es negativo".
-  // Si el número es 0, devuelve false.
-  // Tu código:
+
+  return num > 0;
 }
 
 module.exports = esPositivo;
