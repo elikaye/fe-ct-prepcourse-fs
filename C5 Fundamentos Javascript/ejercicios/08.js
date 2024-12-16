6d8544f5de940d6e4c63e2043db0b7851baf05b8
elikaye <elianakaye13@gmail.com>
2024-12-16 18:03:31 -0300

    Ej8

--- C5 Fundamentos Javascript/ejercicios/08.js
+++ C5 Fundamentos Javascript/ejercicios/08.js
@@ -1,12 +1,6 @@
 function esNumeroEntero(numero) {
-  // La función recibe un argumento llamado numero.
-  // Verifica si este es un número entero o no.
-  // Retorna true si lo es, de lo contrario, retorna false.
-  // Por ejemplo: 
-  // 24 ---> true 
-  // -1212 ---> true 
-  // 121.212 ---> false 
-  // Tu código:
+
+  return Number.isInteger(numero);
 }
 
 module.exports = esNumeroEntero;
