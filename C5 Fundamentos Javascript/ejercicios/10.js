59f58257e835f680d14b4a84b99b15d5070a81e0
elikaye <elianakaye13@gmail.com>
2024-12-16 18:07:35 -0300

    Ej10

--- C5 Fundamentos Javascript/ejercicios/10.js
+++ C5 Fundamentos Javascript/ejercicios/10.js
@@ -1,11 +1,6 @@
 function devolverString(string) {
-  // La función recibe un argumento llamado string.
-  // Debe retornar dicho string.
-  // Por ejemplo: 
-  // "texto" ---> "texto" 
-  // "hola mundo" ---> "hola mundo" 
-  // "SoyHenry" ---> "SoyHenry" 
-  // Tu código:
+ 
+  return string;
 }
 
 module.exports = devolverString;
