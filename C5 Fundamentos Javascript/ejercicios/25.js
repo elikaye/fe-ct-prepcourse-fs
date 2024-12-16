13d4044655e4420667504a902d4a97eb89486800
elikaye <elianakaye13@gmail.com>
2024-12-16 20:33:49 -0300

    Ej25

--- C5 Fundamentos Javascript/ejercicios/25.js
+++ C5 Fundamentos Javascript/ejercicios/25.js
@@ -1,9 +1,6 @@
 function combinarNombres(nombre, apellido) {
-  // La función recibe dos argumentos llamados "nombre" y "apellido" los cuales son string.
-  // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
-  // Por ejemplo: 
-  // "Soy", "Henry" ---> "Soy Henry"
-  // Tu código:
+
+  return nombre + " " + apellido;
 }
 
 module.exports = combinarNombres;
