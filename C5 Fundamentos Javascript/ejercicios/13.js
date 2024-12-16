fe5c20f85105cfd0d3175612a1cb58634115760c
elikaye <elianakaye13@gmail.com>
2024-12-16 18:29:14 -0300

    Ej13

--- C5 Fundamentos Javascript/ejercicios/13.js
+++ C5 Fundamentos Javascript/ejercicios/13.js
@@ -1,11 +1,7 @@
 function menosQueNoventa(num) {
-  // La función recibe un argumento llamado num.
-  // Retorna true si el argumento "num" es menor que noventa.
-  // De lo contrario, retorna false.
-  // Por ejemplo: 
-  // 50 ---> true
-  // 91 ---> false
-  // Tu código:
+  if (num < 90) {
+    
+  }
 }
 
 module.exports = menosQueNoventa;
